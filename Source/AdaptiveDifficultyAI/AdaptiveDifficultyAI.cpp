0222d1dab996af61e25cfc0dc5373b99e81492b7
Sreyash.R.A <rasreyash@gmail.com>
2024-04-24 14:05:14 +0100

    Project files added
    Unreal project files(ver 5.4) added with initially required project settings configured

---
+++ Source/AdaptiveDifficultyAI/AdaptiveDifficultyAI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AdaptiveDifficultyAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AdaptiveDifficultyAI, "AdaptiveDifficultyAI" );
---
+++ Source/AdaptiveDifficultyAI/OneVsOneGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneVsOneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADAPTIVEDIFFICULTYAI_API AOneVsOneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
